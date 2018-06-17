9d46a0e496ae8b97f939574119e7676d3337157a
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-06-17 02:24:49 -0700

    Added setup.py and added pytest to requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='jikanpy',
+   version='1.0',
+   description='Python wrapper for the Jikan API',
+   author='Andrew Conant',
+   packages=['jikanpy'],
+   install_requires=['requests', 'aiohttp'],
+)
