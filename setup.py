71f8388bac315e060deeb5c1fa62fc097a4839dc
Abhinav Kasamsetty <abhinavkasamsetty@Gmail.com>
2018-09-03 11:31:11 -0500

    v1.0.1 - Fixed adding page number as query for URL

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='jikanpy',
-   version='1.0.0',
+   version='1.0.1',
    description='Python wrapper for the Jikan API',
    author='Andrew Conant',
    packages=['jikanpy'],
