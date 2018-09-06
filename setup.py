d37fe884f9def7ef8cc3df145cda51d3a5894ed4
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-09-05 22:05:26 -0500

    v2.0.0 - Updated wrapper to adhere to Jikan REST v3

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
    name='jikanpy',
-   version='1.0.1',
+   version='2.0.0',
    description='Python wrapper for the Jikan API',
-   author='Andrew Conant',
+   author='Andrew Conant, Abhinav Kasamsetty',
    packages=['jikanpy'],
    install_requires=['requests', 'aiohttp'],
 )
