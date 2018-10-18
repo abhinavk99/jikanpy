75048e4cd80b02920abc6613f5a881b201415767
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2018-10-18 14:12:15 -0500

    v2.1.0 - Updated wrapper for Jikan REST v3.1
    Added animelist and mangalist support for user endpoint
    Added method for season archive endpoint

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='jikanpy',
-   version='2.0.0',
+   version='2.1.0',
    description='Python wrapper for the Jikan API',
    author='Andrew Conant, Abhinav Kasamsetty',
    packages=['jikanpy'],
