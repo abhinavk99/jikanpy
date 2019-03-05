61b9477048d5afe7be167529bcb274e2747311f9
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-03-05 15:15:59 -0600

    v2.4.0 - Added option to provide own base url to jikanpy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='jikanpy',
-   version='2.3.2',
+   version='2.4.0',
    description='Python wrapper for the Jikan API',
    author='Andrew Conant, Abhinav Kasamsetty',
    packages=['jikanpy'],
