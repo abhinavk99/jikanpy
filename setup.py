d9e9371292fbb36285eddb5d3cda47888fbc5664
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-03-12 19:17:11 -0500

    v2.4.1 - 100% coverage and abstract method modifications

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='jikanpy',
-   version='2.4.0',
-   description='Python wrapper for the Jikan API',
-   author='Andrew Conant, Abhinav Kasamsetty',
-   packages=['jikanpy'],
-   install_requires=['requests', 'aiohttp'],
+    name='jikanpy',
+    version='2.4.1',
+    description='Python wrapper for the Jikan API',
+    author='Andrew Conant, Abhinav Kasamsetty',
+    packages=['jikanpy'],
+    install_requires=['requests', 'aiohttp'],
 )
