b72dfef3742fc4029102a43500b457772ef22c68
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-03-30 00:52:11 -0500

    v3.0.0 - Type hints, support for 3.6+ only, fixes for search, schedule, meta, manga, user

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jikanpy',
-    version='2.4.1',
+    version='3.0.0',
     description='Python wrapper for the Jikan API',
     author='Andrew Conant, Abhinav Kasamsetty',
     package_data={'jikanpy': ['py.typed']},
