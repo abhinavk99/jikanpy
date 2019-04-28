8dcc2a1ee6aa5405b424149ffb0ab8201ef1dc32
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-04-28 18:21:32 -0500

    v3.0.1 - Handled when no json in response in _check_response

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jikanpy',
-    version='3.0.0',
+    version='3.0.1',
     description='Python wrapper for the Jikan API',
     author='Andrew Conant, Abhinav Kasamsetty',
     package_data={'jikanpy': ['py.typed']},
