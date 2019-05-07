375f2e3a0ae7ec92b310bebf8d256cf9bfc34a75
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-05-07 16:11:20 -0500

    v3.0.2 - genre checking fixes in search method

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jikanpy',
-    version='3.0.1',
+    version='3.0.2',
     description='Python wrapper for the Jikan API',
     author='Andrew Conant, Abhinav Kasamsetty',
     package_data={'jikanpy': ['py.typed']},
