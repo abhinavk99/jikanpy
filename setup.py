b86188889dbbd2b0e520db9bf60d717c041e29c9
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2019-07-23 00:38:00 -0400

    v3.1.2 - Added changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="jikanpy",
-    version="3.1.1",
+    version="3.1.2",
     description="Python wrapper for the Jikan API",
     author="Andrew Conant, Abhinav Kasamsetty",
     package_data={"jikanpy": ["py.typed"]},
