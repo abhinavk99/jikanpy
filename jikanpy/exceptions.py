16cedfeba6080caf9e71abcdb449755acdaff10e
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2020-06-10 03:42:53 -0500

    Revamped APIException to show more info (fixes #79)

--- jikanpy/exceptions.py
+++ jikanpy/exceptions.py
@@ -2,15 +2,50 @@
 ====================================
 exceptions.py contains exceptions used in Jikan and AioJikan.
 """
+from typing import Optional, Mapping, Any, Union
 
 
 class JikanException(Exception):
-    """Base exception class for Jikan.py"""
+    """Base exception class for JikanPy."""
 
 
 class APIException(JikanException):
-    """Exception due to an error response from Jikan API"""
+    """Exception due to an error response from Jikan API.
+
+    Attributes:
+        status_code: HTTP response status code.
+        error_json: JSON error response from Jikan. Defaults to None.
+        relevant_params: Relevant parameters passed into the method resulting in the
+            exception.
+    """
+
+    def __init__(  # pylint: disable=too-many-arguments
+        self,
+        status_code: int,
+        error_json: Optional[Mapping[str, Any]] = None,
+        **relevant_params: Union[int, Optional[str]],
+    ):
+        self.status_code = status_code
+        self.error_json = error_json
+        self.relevant_params = relevant_params
+        super().__init__(self.status_code)
+
+    def __str__(self) -> str:
+        output = f"HTTP {self.status_code}"
+        if self.error_json:
+            error_str = ", ".join(f"{k}={v}" for k, v in self.error_json.items())
+            output += f" - {error_str}"
+        if self.relevant_params:
+            param_str = ", ".join(f"{k}={v}" for k, v in self.relevant_params.items())
+            output += f" for {param_str}"
+        return output
+
+    def __repr__(self) -> str:
+        return (
+            f"APIException(status_code={self.status_code}, "
+            f"error_json={self.error_json}, relevant_params={self.relevant_params})"
+        )
 
 
 class DeprecatedEndpoint(JikanException):
-    """Exception raised when attempting to use deprecated API endpoints"""
+    """Exception raised when attempting to use deprecated API endpoints."""
--- jikanpy/utils.py
+++ jikanpy/utils.py
@@ -8,30 +8,8 @@ from typing import Optional, Dict, Mapping, Union, Any
 import aiohttp
 import requests
 
-from jikanpy.exceptions import APIException
 
 BASE_URL = "https://api.jikan.moe/v3"
-
-
-def check_response(
-    response_dict: Dict[str, Any],
-    response_status_code: int,
-    **kwargs: Union[int, Optional[str]],
-) -> None:
-    """
-        Check if the response is an error
-
-        Keyword Arguments:
-        response_dict -- parsed response from the API call
-                         is empty ({}) when there was a json.decoder.JSONDecodeError
-        response_status -- the corresponding http code for the response
-        kwargs -- keyword arguments
-        """
-    if response_status_code >= 400:
-        error_msg = response_dict.get("error", "")
-        err_str: str = "{} {}: error for ".format(response_status_code, error_msg)
-        err_str += ", ".join("=".join((str(k), str(v))) for k, v in kwargs.items())
-        raise APIException(err_str)
 
 
 def add_jikan_metadata(
@@ -39,7 +17,7 @@ def add_jikan_metadata(
     response_dict: Dict[str, Any],
     url: str,
 ) -> Dict[str, Any]:
-    """Adds the response headers and jikan endpoint url to response dictionary"""
+    """Adds the response headers and jikan endpoint url to response dictionary."""
     response_dict["jikan_url"] = url
     # dict() is to convert from CIMultiDict[str, Any]Proxy for aiohttp.ClientResponse
     response_dict["headers"] = dict(response.headers)
@@ -47,14 +25,14 @@ def add_jikan_metadata(
 
 
 def get_url_with_page(url: str, page: Optional[int], delimiter: str = "/") -> str:
-    """Add page to URL if it exists"""
+    """Adds the page to the URL if it exists."""
     return url if page is None else f"{url}{delimiter}{page}"
 
 
 def get_main_url(
     base_url: str, endpoint: str, id: int, extension: Optional[str], page: Optional[int]
 ) -> str:
-    """Create URL for anime, manga, character, person, and club endpoints"""
+    """Creates the URL for the anime, manga, character, person, and club endpoints."""
     url = f"{base_url}/{endpoint}/{id}"
     if extension is not None:
         url += f"/{extension}"
@@ -65,7 +43,7 @@ def get_main_url(
 def get_creator_url(
     base_url: str, creator_type: str, creator_id: int, page: Optional[int]
 ) -> str:
-    """Create URL for producer and magazine endpoints"""
+    """Creates the URL for the producer and magazine endpoints."""
     url = f"{base_url}/{creator_type}/{creator_id}"
     return get_url_with_page(url, page)
 
@@ -77,7 +55,7 @@ def get_search_url(
     page: Optional[int],
     parameters: Optional[Mapping[str, Optional[Union[int, str, float]]]],
 ) -> str:
-    """Create URL for search endpoint"""
+    """Creates the URL for the search endpoint."""
     url = f"{base_url}/search/{search_type}?q={query}"
     url = get_url_with_page(url, page, delimiter="&page=")
     if parameters is not None:
@@ -86,37 +64,37 @@ def get_search_url(
 
 
 def get_season_url(base_url: str, year: int, season: str) -> str:
-    """Create URL for season endpoint"""
+    """Creates the URL for the season endpoint."""
     return f"{base_url}/season/{year}/{season.lower()}"
 
 
 def get_schedule_url(base_url: str, day: Optional[str]) -> str:
-    """Create URL for schedule endpoint"""
+    """Creates the URL for the schedule endpoint."""
     base_schedule_url = f"{base_url}/schedule"
     return base_schedule_url if day is None else f"{base_schedule_url}/{day.lower()}"
 
 
 def get_season_archive_url(base_url: str) -> str:
-    """Create URL for season archive endpoint"""
+    """Creates the URL for the season archive endpoint."""
     return f"{base_url}/season/archive"
 
 
 def get_season_later_url(base_url: str) -> str:
-    """Create URL for season later endpoint"""
+    """Creates the URL for the season later endpoint."""
     return f"{base_url}/season/later"
 
 
 def get_top_url(
     base_url: str, type: str, page: Optional[int], subtype: Optional[str]
 ) -> str:
-    """Create URL for top endpoint"""
+    """Creates the URL for the top endpoint."""
     url = f"{base_url}/top/{type.lower()}"
     url = get_url_with_page(url, page)
     return url if subtype is None else f"{url}/{subtype.lower()}"
 
 
 def get_genre_url(base_url: str, type: str, genre_id: int, page: Optional[int]) -> str:
-    """Create URL for genre endpoint"""
+    """Creates the URL for the genre endpoint."""
     url = f"{base_url}/genre/{type.lower()}/{genre_id}"
     return get_url_with_page(url, page)
 
@@ -129,7 +107,7 @@ def get_user_url(
     page: Optional[int],
     parameters: Optional[Mapping[str, Any]],
 ) -> str:
-    """Create URL for user endpoint"""
+    """Creates the URL for the user endpoint."""
     url = f"{base_url}/user/{username.lower()}"
     if request is not None:
         url += f"/{request}"
@@ -149,7 +127,7 @@ def get_meta_url(
     period: Optional[str],
     offset: Optional[int],
 ) -> str:
-    """Create URL for meta endpoint"""
+    """Creates the URL for the meta endpoint."""
     url = f"{base_url}/meta/{request}"
     if type is not None and period is not None:
         url += f"/{type}/{period}"
